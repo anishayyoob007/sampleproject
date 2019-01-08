983ea5d84393e288ad7512f8ad89813ca4f443b1
anishayyoob007 <46504710+anishayyoob007@users.noreply.github.com>
2019-01-08 23:18:11 +0100

    Add files

---
+++ Hello/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many hours of sleep you get last night ? ");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+            string sleepStatus = "";
+            if(hoursOfSleep > 7)
+            {
+                sleepStatus = "Good Sleep";
+            }
+            else
+            {
+                sleepStatus = "Bad Sleep";
+            }
+
+            Console.WriteLine(" Got " + sleepStatus);
+           
+            Console.ReadKey();
+        }
+    }
+}
